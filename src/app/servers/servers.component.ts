58a8be4f01549c3253b17533bf46b9d9cf62dbbe
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-12 11:25:53 +0700

    Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
@@ -17,6 +18,10 @@ export class ServersComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  onCreatedServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
