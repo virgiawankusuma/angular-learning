ce79a2990f046bd04b14826a6008f744abb239e9
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-12 11:06:47 +0700

    String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
