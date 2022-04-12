d113a88b129f3d08a9eb26efaf9b824a23627726
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-12 12:12:12 +0700

    Applying CSS Classes Dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: ['./server.component.css']
+    styles:[`
+        .online{
+            color:white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId = 10;
